fd4d5b8f3641200c8a72d8ff206228c5443fa5e7
lucasfmerino <lucasfmerino@gmail.com>
2024-12-01 23:26:10 -0300

    refactor(ToeknService): added roles in JWT

--- src/main/java/com/arca/auth_service/modules/auth/service/TokenService.java
+++ src/main/java/com/arca/auth_service/modules/auth/service/TokenService.java
@@ -1,5 +1,6 @@
 package com.arca.auth_service.modules.auth.service;
 
+import com.arca.auth_service.modules.role.Role;
 import com.arca.auth_service.modules.user.domain.model.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TokenService
@@ -29,10 +32,15 @@ public class TokenService
         {
             Algorithm algorithm = Algorithm.HMAC256(secret);
 
+            List<String> roles = user.getRoles().stream()
+                    .map(Role::name)
+                    .toList();
+
             return JWT.create()
                     .withIssuer("arca-api")
                     .withSubject(user.getUsername())
                     .withClaim("id", user.getId().toString())
+                    .withClaim("roles", roles)
                     .withExpiresAt(generateExpirationDate())
                     .sign(algorithm);
         }
@@ -84,6 +92,25 @@ public class TokenService
         }
         catch (JWTVerificationException exception)
         {
+            throw new RuntimeException("Invalid or expired JWT Token!", exception);
+        }
+    }
+
+
+    /*
+     * VALIDATE TOKEN (GET ROLES)
+     *
+     */
+    public List<String> validateTokenClaimRoles(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer("arca-api")
+                    .build()
+                    .verify(token)
+                    .getClaim("roles").asList(String.class);
+        } catch (JWTVerificationException exception) {
             throw new RuntimeException("Invalid or expired JWT Token!", exception);
         }
     }
